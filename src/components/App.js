3f5fe66b17df05ef1d7b547dcb1441e6dcbf1589
Raghav <raghavtonk13@gmail.com>
2024-03-06 21:55:32 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [name,setName] = useState("");
+  function formHandle(event){
+    setName(event.target.value );
+
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form >
+          <label htmlFor="name">Enter your name:</label><br/>
+          <input onChange={formHandle} name="name" value={name}></input>
+        </form>
+        {!name && <p></p>}
+        {name &&<p>Hello {name}!</p>}
     </div>
   )
 }
